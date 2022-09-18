7f00a4468311f51091218aa8fe0fe0d9df5b004f
Pavithra G <112771860+pavithra11297@users.noreply.github.com>
2022-09-18 14:18:49 +0530

    Update MultipleLanguage.java

--- assignment/MultipleLanguage.java
+++ assignment/MultipleLanguage.java
@@ -1,6 +1,6 @@
 package week3.day1.assignment;
 
-public abstract class MultipleLanguage {
+public abstract class MultipleLanguage implements Language,TestTool  {
 	public void python() 
 	{
 		System.out.println("This is python method in MultipleLanguage abstract class ");
